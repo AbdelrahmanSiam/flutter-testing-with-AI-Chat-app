0c20cac36ff6997ea22aa8facecf3303fbf5f5fa
abdelrahmanahmed <syamabdo382@gmail>
2026-07-14 07:17:25 +0300

    Provide GeminiSendMessageCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:ai_chat_app/cubit/gemini_send_message/gemini_send_message_cubit.dart';
+import 'package:ai_chat_app/repositories/gemini_send_message_repository_impl.dart';
+import 'package:ai_chat_app/services/client_api.dart';
+import 'package:ai_chat_app/services/gemini_chat_service.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'ui/screens/chat_screen.dart';
 
 void main() {
@@ -18,7 +22,19 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
       ),
-      home: const ChatScreen(),
+      home: BlocProvider(
+        create: (_) => GeminiSendMessageCubit(
+          repository: GeminiSendMessageRepositoryImpl(
+            service: GeminiChatService(
+              api: ClientApi(
+                baseUrl: 'https://generativelanguage.googleapis.com',
+              ),
+              apiKey: '',
+            ),
+          ),
+        ),
+        child: const ChatScreen(),
+      ),
     );
   }
 }
